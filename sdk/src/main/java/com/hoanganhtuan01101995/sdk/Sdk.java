5d2d37ea70f2fe7f379d7a532a2ea525a24ad8d3
Hoang Anh Tuan <hoanganhtuan01101995@gmai.com>
2017-11-12 10:17:13 +0700

    push

--- sdk/src/main/java/com/hoanganhtuan01101995/sdk/Sdk.java
+++ sdk/src/main/java/com/hoanganhtuan01101995/sdk/Sdk.java
@@ -11,6 +11,18 @@ import com.hoanganhtuan01101995.sdk.api.Api;
 
 public class Sdk {
 
+//    public static final int INTERSTITIAL_KEY = R.string.interstitialKey;
+//    public static final int REWARDED_VIDEO_KEY = R.string.rewardedVideoKey;
+//    public static final String APP_KEY = "AIzaSyAmf_ICIgnjUuSKTFx4A_fTNiH6eeIHVxg";
+//    public static final String BROWSER_KEY = "AIzaSyCqpXAb5VH8RdhjUurFtUJ7Z7IpuorQvkE";
+//    public static final String CHANNEL_ID = "UCAuUUnT6oDeKwE6v1NGQxug";
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        Sdk.instance(this, APP_KEY, BROWSER_KEY, INTERSTITIAL_KEY, REWARDED_VIDEO_KEY, CHANNEL_ID);
+//    }
+
     public static int INTERSTITIAL_KEY;
     public static int REWARDED_VIDEO_KEY;
     public static String APP_KEY;
